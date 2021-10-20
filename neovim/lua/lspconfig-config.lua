f53092d24427720aa689fdb1698d8f3625ba6e74
Nikolai Oplachko <magnickolas@gmail.com>
2021-10-20 18:28:09 +0300

    update neovim config

--- neovim/lua/lspconfig-config.lua
+++ neovim/lua/lspconfig-config.lua
@@ -2,10 +2,21 @@ local lspconfig  = require('lspconfig')
 local lspinstall = require('lspinstall')
 lspinstall.setup()
 
-local servers = lspinstall.installed_servers()
-for _, server in pairs(servers) do
-  lspconfig[server].setup{capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())}
-end 
+local function setup_servers()
+  lspinstall.setup()
+  local servers = lspinstall.installed_servers()
+  for _, server in pairs(servers) do
+    lspconfig[server].setup{capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())}
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+lspinstall.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
 --lspconfig.pyright.setup{}
 --lspconfig.rust_analyzer.setup{
 --    settings = {
