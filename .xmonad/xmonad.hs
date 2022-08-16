699b7ea2c06098643daa7d59e0da1b4209a728a2
Nikolai Oplachko <magnickolas@gmail.com>
2022-08-16 18:08:40 +0300

    update xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,34 +1,64 @@
 import qualified Codec.Binary.UTF8.String as UTF8
+import Control.Monad (when)
 import qualified DBus as D
 import qualified DBus.Client as D
+import Data.Foldable (find)
 import qualified Data.Map as M
 import Data.Maybe (fromJust)
+import Data.Monoid (All (All))
+import Foreign.C (CInt)
 import Graphics.X11.ExtraTypes.XF86
 import Graphics.X11.Types
 import System.Exit (exitSuccess)
 import XMonad (
     Default (def),
-    KeyMask,
-    KeySym,
+    Event (MotionEvent, ev_x, ev_y),
     Layout,
+    Rectangle,
+    ScreenDetail,
+    ScreenId,
+    WorkspaceId,
     X,
-    XConfig (XConfig, borderWidth, focusFollowsMouse, keys, layoutHook, logHook, manageHook, modMask, startupHook, terminal, workspaces),
+    XConfig (
+        XConfig,
+        borderWidth,
+        focusFollowsMouse,
+        keys,
+        layoutHook,
+        logHook,
+        manageHook,
+        modMask,
+        rootMask,
+        startupHook,
+        terminal,
+        workspaces
+    ),
     className,
+    get,
+    gets,
     handleEventHook,
     io,
     kill,
+    rect_height,
+    rect_width,
+    rect_x,
+    rect_y,
+    screenRect,
     sendMessage,
     spawn,
     title,
     windows,
+    windowset,
     withFocused,
     xmonad,
     (.|.),
     (<+>),
     (=?),
  )
+import XMonad.Actions.CycleWS
 import XMonad.Actions.NoBorders (toggleBorder)
 import XMonad.Config.Desktop (desktopConfig)
+import qualified XMonad.Core
 import XMonad.Hooks.DynamicLog (
     PP (ppCurrent, ppHidden, ppOutput, ppSep, ppSort, ppTitle, ppUrgent, ppVisible, ppWsSep),
     filterOutWsPP,
@@ -38,7 +68,6 @@ import XMonad.Hooks.DynamicLog (
 import XMonad.Hooks.EwmhDesktops (
     ewmh,
     ewmhFullscreen,
-    fullscreenEventHook,
  )
 import XMonad.Hooks.InsertPosition (
     Focus (Newer),
@@ -51,20 +80,24 @@ import XMonad.Hooks.ManageDocks (
     docks,
     manageDocks,
  )
+import XMonad.Hooks.ManageHelpers (
+    isDialog,
+ )
 import XMonad.Hooks.RefocusLast (
     refocusLastLayoutHook,
-    refocusLastLogHook,
     refocusLastWhen,
-    refocusWhen,
     refocusingIsActive,
     shiftRLWhen,
-    toggleFocus,
+ )
+import XMonad.Hooks.Rescreen (
+    RescreenConfig,
+    afterRescreenHook,
+    randrChangeHook,
+    rescreenHook,
  )
 import XMonad.Hooks.StatusBar.PP (dynamicLogString)
 import XMonad.Layout (
-    ChangeLayout (NextLayout),
     Resize (Expand, Shrink),
-    Tall (Tall),
     (|||),
  )
 import XMonad.Layout.Decoration (
@@ -73,10 +106,9 @@ import XMonad.Layout.Decoration (
     shrinkText,
  )
 import qualified XMonad.Layout.LayoutModifier
-import XMonad.Layout.LimitWindows (limitWindows)
 import XMonad.Layout.MultiToggle (EOT (EOT), mkToggle, (??))
 import qualified XMonad.Layout.MultiToggle as MT (Toggle (Toggle))
-import XMonad.Layout.MultiToggle.Instances (StdTransformers (MIRROR, NBFULL, NOBORDERS))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers (NBFULL, NOBORDERS))
 import XMonad.Layout.NoBorders (noBorders)
 import XMonad.Layout.Renamed (Rename (Replace), renamed)
 import XMonad.Layout.ResizableTile (
@@ -94,13 +126,9 @@ import XMonad.Layout.TabBarDecoration (
     resizeVertical,
     tabBar,
  )
-import XMonad.Layout.TrackFloating (
-    trackFloating,
-    useTransientFor,
- )
 import XMonad.ManageHook (
     composeAll,
-    doFloat,
+    doF,
     doShift,
     resource,
     (-->),
@@ -108,19 +136,15 @@ import XMonad.ManageHook (
  )
 import XMonad.Prompt (autoComplete, font)
 import XMonad.Prompt.ConfirmPrompt (confirmPrompt)
-import XMonad.Prompt.Shell (shellPrompt)
 import XMonad.Prompt.Window (
-    WindowPrompt (Bring, Goto),
+    WindowPrompt (Bring),
     allWindows,
     windowPrompt,
-    wsWindows,
  )
 import qualified XMonad.StackSet as W
-import XMonad.Util.Image (Placement (CenterLeft, CenterRight))
 import XMonad.Util.NamedScratchpad (
     NamedScratchpad (NS),
     customFloating,
-    defaultFloating,
     namedScratchpadAction,
     namedScratchpadManageHook,
     nsHideOnFocusLoss,
@@ -129,6 +153,7 @@ import XMonad.Util.NamedScratchpad (
 import XMonad.Util.Run (safeSpawn)
 import XMonad.Util.SpawnOnce (spawnOnce)
 
+main :: IO ()
 main = do
     dbus <- D.connectSession
     D.requestName
@@ -137,39 +162,89 @@ main = do
         [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
     runXMonad dbus
 
+runXMonad :: D.Client -> IO ()
 runXMonad dbus =
     xmonad $
-        docks $
-            ewmhFullscreen $
-                ewmh
-                    desktopConfig
-                        { terminal = myTerminal
-                        , modMask = mod4Mask
-                        , borderWidth = 0
-                        , focusFollowsMouse = False
-                        , startupHook =
-                            do
-                                spawnOnce clipboardManager
-                                spawnOnce screenshoter
-                                spawnOnce compositor
-                                spawnOnce udiskie
-                                spawn setupKeyboard
-                                spawn setupMonitor
-                                spawn setWallpaper
-                                spawn myBar
-                        , handleEventHook =
-                            refocusLastWhen refocusingIsActive <+> handleEventHook def
-                        , layoutHook =
-                            refocusLastLayoutHook $ trackFloating (useTransientFor myLayoutHook)
-                        , keys = refocusLastKeys <+> myKKeys <+> keys desktopConfig
-                        , logHook =
-                            nsHideOnFocusLoss myScratchpads
-                                <+> (dynamicLogWithPPUTF8 . filterOutWsPP [scratchpadWorkspaceTag])
-                                    (myLogHook dbus)
-                        , manageHook = myManageHook
-                        , workspaces = myWorkspaces
-                        }
+        rescreenHook rescreenCfg $
+            docks $
+                ewmhFullscreen $
+                    ewmh
+                        desktopConfig
+                            { terminal = myTerminal
+                            , modMask = mod4Mask
+                            , rootMask = rootMask def .|. pointerMotionMask
+                            , borderWidth = 0
+                            , focusFollowsMouse = True
+                            , startupHook =
+                                do
+                                    spawnOnce clipboardManager
+                                    spawnOnce screenshoter
+                                    spawnOnce compositor
+                                    spawnOnce udiskie
+                                    spawnOnce screensaverBg
+                                    spawn setupKeyboard
+                                    spawn setWallpaper
+                                    spawn myBar
+                            , handleEventHook =
+                                handleEventHook def
+                                    <+> multiScreenFocusHook
+                                    <+> refocusLastWhen refocusingIsActive
+                            , layoutHook =
+                                refocusLastLayoutHook myLayoutHook
+                            , keys = refocusLastKeys <+> myKKeys <+> keys desktopConfig
+                            , logHook =
+                                nsHideOnFocusLoss myScratchpads
+                                    <+> (dynamicLogWithPPUTF8 . filterOutWsPP [scratchpadWorkspaceTag])
+                                        (myLogHook dbus)
+                            , manageHook = myManageHook
+                            , workspaces = myWorkspaces
+                            }
 
+multiScreenFocusHook :: Event -> X All
+multiScreenFocusHook MotionEvent{ev_x = x, ev_y = y} = do
+    ms <- getScreenForPos x y
+    case ms of
+        Just cursorScreen -> do
+            let cursorScreenID = W.screen cursorScreen
+            focussedScreenID <- gets (W.screen . W.current . windowset)
+            when (cursorScreenID /= focussedScreenID) (focusWS $ W.tag $ W.workspace cursorScreen)
+            return (All True)
+        _ -> return (All True)
+  where
+    getScreenForPos ::
+        CInt ->
+        CInt ->
+        X (Maybe (W.Screen WorkspaceId (Layout Window) Window ScreenId ScreenDetail))
+    getScreenForPos sx sy = do
+        ws <- windowset <$> get
+        let screens = W.current ws : W.visible ws
+            inRects = map (inRect sx sy . screenRect . W.screenDetail) screens
+        return $ fst <$> find snd (zip screens inRects)
+    inRect :: CInt -> CInt -> Rectangle -> Bool
+    inRect rx ry rect =
+        let l = fromIntegral (rect_x rect)
+            r = l + fromIntegral (rect_width rect)
+            t = fromIntegral (rect_y rect)
+            b = t + fromIntegral (rect_height rect)
+         in rx >= l && rx < r && ry >= t && ry < b
+    focusWS :: WorkspaceId -> X ()
+    focusWS wsid = windows (W.view wsid)
+multiScreenFocusHook _ = return (All True)
+
+myAfterRescreenHook :: X ()
+myAfterRescreenHook = return ()
+
+myRandrChangeHook :: X ()
+myRandrChangeHook = spawn setupMonitor
+
+rescreenCfg :: RescreenConfig
+rescreenCfg =
+    def
+        { afterRescreenHook = myAfterRescreenHook
+        , randrChangeHook = myRandrChangeHook
+        }
+
+refocusLastKeys :: XConfig l -> M.Map (KeyMask, KeySym) (X ())
 refocusLastKeys cnf =
     M.fromList $
         [ ( (modMask cnf .|. shiftMask, n)
@@ -178,16 +253,19 @@ refocusLastKeys cnf =
         | (n, wksp) <- zip [xK_1 .. xK_9] (workspaces cnf)
         ]
 
+myManageHook :: XMonad.Core.ManageHook
 myManageHook =
     insertPosition Below Newer
         <+> composeAll
             [ (className =? "firefox" <&&> resource =? "Dialog") --> centerWin
             , (className =? "Pavucontrol") --> centerWin
             , (title =? "Telegram") --> doShift (myWorkspaces !! 8)
+            , isDialog --> doF W.swapUp
             ]
         <+> manageDocks
         <+> namedScratchpadManageHook myScratchpads
 
+myScratchpads :: [NamedScratchpad]
 myScratchpads =
     [ NS
         scratchpadTerminalTitle
@@ -206,73 +284,88 @@ dynamicLogWithPPUTF8 pp =
     dynamicLogString pp
         >>= (io . ppOutput pp) . UTF8.encodeString
 
+centerWin :: XMonad.Core.ManageHook
 centerWin = customFloating $ W.RationalRect (1 / 8) (1 / 8) (3 / 4) (3 / 4)
 
+centerWinBig :: XMonad.Core.ManageHook
 centerWinBig = customFloating $ W.RationalRect (1 / 16) (1 / 16) (7 / 8) (7 / 8)
 
+scratchpadTerminalTitle :: String
 scratchpadTerminalTitle = "scratchpad_terminal"
 
+scratchpadTerminalAdditionalOptions :: String
 scratchpadTerminalAdditionalOptions = " -o window.opacity=1.0 "
 
+toggleSP :: String -> X ()
 toggleSP = namedScratchpadAction myScratchpads
 
 myKKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
-myKKeys XConfig{modMask = modMask} =
+myKKeys XConfig{modMask = winMask} =
     M.fromList $
-        [ ((modMask, xK_Return), spawn myTerminal)
-        , ((modMask .|. shiftMask, xK_Return), windows W.swapMaster)
-        , ((modMask, xK_d), spawn dmenu)
-        , ((modMask, xK_i), spawn dmenuApp)
+        [ ((winMask, xK_Return), spawn myTerminal)
+        , ((winMask .|. shiftMask, xK_Return), windows W.swapMaster)
+        , ((winMask, xK_d), spawn dmenu)
+        , ((winMask, xK_i), spawn dmenuApp)
         , ((controlMask .|. shiftMask, xK_4), spawn takeScreenshot)
+        , ((winMask .|. shiftMask, xK_s), spawn takeScreenshot)
         , ((controlMask .|. shiftMask, xK_3), spawn screenZoomer)
-        , ((modMask .|. shiftMask, xK_t), withFocused toggleBorder)
-        , ((modMask .|. shiftMask, xK_u), spawn suspend)
-        , ((modMask .|. shiftMask, xK_q), kill)
+        , ((winMask .|. shiftMask, xK_t), withFocused toggleBorder)
+        , ((winMask .|. shiftMask, xK_u), spawn suspend)
+        , ((winMask .|. shiftMask, xK_x), spawn (switchLayout US) <+> spawn screensaver)
+        , ((winMask .|. shiftMask, xK_q), kill)
         ,
             ( (mod1Mask .|. shiftMask, xK_e)
             , confirmPrompt def{font = myFont} "exit" $ io exitSuccess
             )
         ,
-            ( (modMask, xK_f)
+            ( (winMask, xK_f)
             , sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts
             )
-        , ((modMask, xK_w), sendMessage NextLayout)
-        , ((modMask, xK_o), safeSpawn browser [])
-        , ((modMask, xK_minus), toggleSP scratchpadTerminalTitle)
-        , ((modMask, xK_equal), toggleSP spotifyQt)
-        ,
-            ( (modMask, xK_g)
-            , spawn gotoWindow
-            )
-        ,   ( (modMask, xK_b)
-            , windowPrompt def{font = myFont, autoComplete = Just 0} Bring allWindows
-            )
-        , ((modMask .|. shiftMask, xK_x), spawn (switchLayout US) <+> spawn lockScreen <+> spawn suspend)
-        , ((modMask, xK_Left), sendMessage Shrink)
-        , ((modMask, xK_Up), sendMessage MirrorExpand)
-        , ((modMask, xK_Right), sendMessage Expand)
-        , ((modMask, xK_Down), sendMessage MirrorShrink)
+        , ((winMask, xK_o), safeSpawn browser [])
+        , ((winMask, xK_minus), toggleSP scratchpadTerminalTitle)
+        , ((winMask, xK_equal), toggleSP spotifyQt)
+        , ((winMask, xK_g), spawn gotoWindow)
+        , ((winMask, xK_b), windowPrompt def{font = myFont, autoComplete = Just 0} Bring allWindows)
+        , ((winMask .|. shiftMask, xK_m), spawn setupKeyboard <+> spawn setupMonitor <+> spawn setWallpaper)
+        , ((winMask, xK_Left), sendMessage Shrink)
+        , ((winMask, xK_Up), sendMessage MirrorExpand)
+        , ((winMask, xK_Right), sendMessage Expand)
+        , ((winMask, xK_Down), sendMessage MirrorShrink)
         , ((0, xF86XK_AudioMute), spawn audioToggle)
         , ((0, xF86XK_AudioRaiseVolume), spawn raiseVolume)
         , ((0, xF86XK_AudioLowerVolume), spawn lowerVolume)
         , ((0, xF86XK_MonBrightnessUp), spawn brightnessUp)
         , ((0, xF86XK_MonBrightnessDown), spawn brightnessDown)
+        , ((winMask, xK_Down), nextWS)
+        , ((winMask, xK_Up), prevWS)
+        , ((winMask .|. shiftMask, xK_Down), shiftToNext)
+        , ((winMask .|. shiftMask, xK_Up), shiftToPrev)
+        , ((winMask, xK_Right), nextScreen)
+        , ((winMask, xK_Left), prevScreen)
+        , ((winMask .|. shiftMask, xK_Right), shiftNextScreen)
+        , ((winMask .|. shiftMask, xK_Left), shiftPrevScreen)
+        , ((winMask, xK_z), toggleWS)
         ]
-            ++ [((modMask, key), windows $ W.greedyView ws) | (key, ws) <- myExtraWorkspaces]
-            ++ [ ((modMask .|. shiftMask, key), windows $ W.shift ws)
+            ++ [((winMask, key), windows $ W.greedyView ws) | (key, ws) <- myExtraWorkspaces]
+            ++ [ ((winMask .|. shiftMask, key), windows $ W.shift ws)
                | (key, ws) <- myExtraWorkspaces
                ]
 
+myExtraWorkspaces :: [(KeySym, String)]
 myExtraWorkspaces = [(xK_0, "[0]")]
 
+myWorkspaces :: [String]
 myWorkspaces =
     ["DEV [1]", "WWW [2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "IM [9]"]
         ++ map snd myExtraWorkspaces
+
+myWorkspaceIndices :: M.Map String Integer
 myWorkspaceIndices = M.fromList $ zip myWorkspaces [1 ..]
 
-clickable ws = "%{A1:xdotool key super+" ++ show (index i) ++ ":}" ++ ws ++ "%{A}"
+clickable :: [Char] -> [Char]
+clickable ws = "%{A1:xdotool key super+" ++ show (index idx) ++ ":}" ++ ws ++ "%{A}"
   where
-    i = fromJust $ M.lookup ws myWorkspaceIndices
+    idx = fromJust $ M.lookup ws myWorkspaceIndices
     index i
         | i == 10 = 0
         | otherwise = i
@@ -307,9 +400,11 @@ mySpacing' ::
     Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing' i = spacingRaw True (Border i i i i) True (Border i i i i) True
 
-myFontSize s = "xft:Terminus-" ++ show s ++ ":style=bold"
+myFontSize :: Show a => a -> [Char]
+myFontSize s = "xft:TerminessTTF Nerd Font Mono-" ++ show s ++ ":style=bold"
 
-myFont = myFontSize 12
+myFont :: [Char]
+myFont = myFontSize (12 :: Integer)
 
 baseTheme :: Theme
 baseTheme =
@@ -357,49 +452,51 @@ instance Show LayoutT where
         RU -> "ru"
 
 -- Commands
-audioToggle      = "pactl set-sink-mute @DEFAULT_SINK@ toggle"
-brightnessDown   = "~/scripts/change_brightness.sh dec"
-brightnessUp     = "~/scripts/change_brightness.sh inc"
-browser          = "firefox"
+audioToggle = "pactl set-sink-mute 0 toggle"
+brightnessDown = "~/scripts/change_brightness.sh dec"
+brightnessUp = "~/scripts/change_brightness.sh inc"
+browser = "firefox"
 clipboardManager = "parcellite"
-compositor       = "picom"
-dmenu            = "dmenu_run"
-dmenuApp         = "rofi -show combi -combi-modi 'window,run,ssh' -modi combi"
-gotoWindow       = "rofi run -show"
-lockScreen       = "~/scripts/lockscreen.sh"
-lowerVolume      = "pactl set-sink-volume @DEFAULT_SINK@ -5%"
-myBar            = "~/.config/polybar/launch.sh"
-myTerminal       = "alacritty"
-raiseVolume      = "pactl set-sink-volume @DEFAULT_SINK@ +5%"
-screenZoomer     = "boomer"
-screenshoter     = "flameshot"
-setWallpaper     = "~/scripts/set_wallpaper.sh"
-setupKeyboard    = "~/scripts/setup_keyboard.sh"
-setupMonitor     = "~/scripts/setup_monitor.sh"
-spotifyQt        = "spotify-qt"
-suspend          = "systemctl suspend"
+compositor = "picom"
+dmenu = "dmenu_run"
+dmenuApp = "rofi -show combi -combi-modi 'window,run,ssh,drun' -modi combi -show-icons"
+gotoWindow = "rofi run -show"
+lockScreen = "~/scripts/lockscreen.sh"
+lowerVolume = "pactl set-sink-volume @DEFAULT_SINK@ -5%"
+myBar = "~/.config/polybar/launch.sh"
+myTerminal = "alacritty"
+raiseVolume = "pactl set-sink-volume @DEFAULT_SINK@ +5%"
+screenZoomer = "boomer"
+screenshoter = "flameshot"
+screensaverBg = "xscreensaver -no-splash"
+screensaver = "xscreensaver-command -lock"
+setWallpaper = "~/scripts/set_wallpaper.sh"
+setupKeyboard = "~/scripts/setup_keyboard.sh"
+setupMonitor = "~/scripts/setup_monitor.sh"
+spotifyQt = "spotify-qt"
+suspend = "systemctl suspend"
 switchLayout :: LayoutT -> String
-switchLayout     = ("xkb-switch -s " ++) . show
-takeScreenshot   = screenshoter ++ " gui"
-udiskie          = "udiskie"
+switchLayout = ("xkb-switch -s " ++) . show
+takeScreenshot = screenshoter ++ " gui"
+udiskie = "udiskie"
 
 -- Colors
-base00    = "#657b83"
-base01    = "#586e75"
-base02    = "#073642"
-base03    = "#002b36"
-fg        = "#ebdbb2"
-bg        = "#282828"
-gray      = "#a89984"
-bg1       = "#3c3836"
-bg2       = "#504945"
-bg3       = "#665c54"
-bg4       = "#7c6f64"
-green     = "#b8bb26"
+base00 = "#657b83"
+base01 = "#586e75"
+base02 = "#073642"
+base03 = "#002b36"
+fg = "#ebdbb2"
+bg = "#282828"
+gray = "#a89984"
+bg1 = "#3c3836"
+bg2 = "#504945"
+bg3 = "#665c54"
+bg4 = "#7c6f64"
+green = "#b8bb26"
 darkgreen = "#98971a"
-red       = "#fb4934"
-darkred   = "#cc241d"
-yellow    = "#fabd2f"
-blue      = "#83a598"
-purple    = "#d3869b"
-aqua      = "#8ec07c"
+red = "#fb4934"
+darkred = "#cc241d"
+yellow = "#fabd2f"
+blue = "#83a598"
+purple = "#d3869b"
+aqua = "#8ec07c"
