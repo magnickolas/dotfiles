4f0b00bd43776cec261ba4e439041e6b9cf47529
Nikolai Oplachko <magnickolas@gmail.com>
2023-01-12 00:42:53 +0400

    update tmux status script

--- .tmux/right_status.sh
+++ .tmux/right_status.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
+function ok() {
+	[[ -n "${1// /}" ]]
+}
+
+# --- Widgets ---
 function git_branch() {
-	local branch
 	branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 	if [[ $? -eq 0 ]]; then
-		printf "%s %s" " " "$branch"
+		printf "%s %s" " " "${branch}"
+	fi
+}
+
+function pomo() {
+	status=$(x pomo)
+	if [[ $? -eq 0 ]] && ok "${status}"; then
+		printf "%s" "${status}"
 	fi
 }
 
 function run() {
-	cd "$1"
-	git_branch && printf " | "
-	x pomo && printf " "
+	cd "$1" || exit
+	git_branch_out=$(git_branch)
+	pomo_out=$(pomo)
+	first=0
+	if ok "${git_branch_out}"; then
+		first=1
+		printf "%s" "${git_branch_out}"
+	fi
+	if ok "${pomo_out}"; then
+		if [[ $first -eq 1 ]]; then
+			printf " | "
+		fi
+		printf "%s" "${pomo_out}"
+	fi
+	printf " "
 }
 
 run "$@"
